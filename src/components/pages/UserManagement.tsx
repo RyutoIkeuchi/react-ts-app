4c30255f9d512753a251639714d377e995d4ca8d
Ikeuchi Ryuto <ryu.i7133@gmail.com>
2021-04-23 22:00:45 +0900

    ユーザー一覧の作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,31 @@
-import { VFC,memo } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Wrap, WrapItem, Spinner, Center } from '@chakra-ui/react';
+import { VFC, memo, useEffect } from 'react';
+import { useAllUsers } from '../hooks/useAllUsers';
+import { UserCard } from '../organisms/user/UserCard';
 
 export const UserManagement: VFC = memo(() => {
-	return <p>ユーザー管理ページです</p>;
+	const { getUsers, users, loading } = useAllUsers();
+	useEffect(() => getUsers(), []);
+	return (
+		<>
+			{loading ? (
+				<Center h="100vh">
+					<Spinner />
+				</Center>
+			) : (
+				<Wrap p={{ base: 4, md: 10 }}>
+					{users.map((user) => (
+						<WrapItem key={user.id} mx='auto'>
+							<UserCard
+								imageUrl="https://source.unsplash.com/random"
+								userName={user.username}
+								fullName={user.name}
+							/>
+						</WrapItem>
+					))}
+				</Wrap>
+			)}
+		</>
+	);
 });
